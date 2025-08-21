7522aeef7a33e35b445836147d43d7aa5e6e8968
Clayson Reis <claysonreis28@gmail.com>
2025-08-21 00:25:31 -0300

    Primeiro commit - Desafio controle de fluxo

---
+++ src/Contador/Main.java
@@ -0,0 +1,23 @@
+package Contador;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = terminal.nextInt();
+
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            Contador.contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        terminal.close();
+    }
+}
